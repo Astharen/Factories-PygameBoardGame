4b8c7bd6e119265559692cf1c2184299fdd614ce
AlmudenaZhou <almudena.zhou.rl@gmail.com>
2023-11-24 00:56:10 +0100

    [FEATURE] Implemented get_position_tile in BoardModel

--- modules/board_model.py
+++ modules/board_model.py
@@ -2,12 +2,14 @@ import random
 
 from data.constants import board_size, n_divisions
 from modules.board import Board
+from modules.tile import Tile
 
 
 class BoardModel(Board):
 
     def __init__(self):
         self.tile_mapping = []
+        self.wood_squares = self.calc_wood_squares()
 
     @staticmethod
     def calc_wood_squares():
@@ -18,8 +20,11 @@ class BoardModel(Board):
                                           int(board_size[0] * board_size[1] / 2 / n_divisions))
         return wood_squares
 
-    def add_tile(self):
-        pass
+    def get_position_tile(self, x, y):
+        if (board_size[0] * y + x + 1) in self.wood_squares:
+            return Tile(x, y, "1")
+        else:
+            return Tile(x, y, "0")
 
     @staticmethod
     def first_position():
