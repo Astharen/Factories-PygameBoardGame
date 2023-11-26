c8c6ec75c0e41c5dc55a619a70239f4d59fde75d
AlmudenaZhou <almudena.zhou.rl@gmail.com>
2023-11-26 13:34:31 +0100

    [REFACTOR] BoardView from Board class

--- modules/board_view.py
+++ modules/board_view.py
@@ -1,12 +1,13 @@
 from data.constants import board_size, window_x, window_y, square_size, space_between_squares
+from modules.board import Board
 from modules.tile_view import TileView
 
 
-class BoardView:
+class BoardView(Board):
 
     def __init__(self):
         self.left_top_y = window_y - board_size[1] * (square_size + space_between_squares) - window_x / 100
-        self.tile_mapping = {}
+        self.tile_mapping = []
 
     def get_position_tile(self, x, y):
         return TileView(x, y, self.left_top_y)
