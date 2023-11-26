00e4c581d10d44b4774d65cccc2d403de91c455c
AlmudenaZhou <almudena.zhou.rl@gmail.com>
2023-11-26 13:33:16 +0100

    [FEATURE] Added BoardView and TileView

--- modules/board_view.py
+++ modules/board_view.py
@@ -1,7 +1,12 @@
 from data.constants import board_size, window_x, window_y, square_size, space_between_squares
+from modules.tile_view import TileView
 
 
 class BoardView:
 
     def __init__(self):
         self.left_top_y = window_y - board_size[1] * (square_size + space_between_squares) - window_x / 100
+        self.tile_mapping = {}
+
+    def get_position_tile(self, x, y):
+        return TileView(x, y, self.left_top_y)
--- modules/tile_view.py
+++ modules/tile_view.py
@@ -1,6 +1,12 @@
+import pygame
+
+from data.constants import window_x, space_between_squares, square_size
+
+
 class TileView:
 
-    def __init__(self, x, y):
-
-        self.rect = pygame.Rect(window_x/100 + space_between_squares + x * square_size + space_between_squares * x,
-             left_top_y + y * square_size + space_between_squares * y, square_size, square_size)
+    def __init__(self, x, y, left_top_y):
+        self.x = x
+        self.y = y
+        self.rect = pygame.Rect(window_x / 100 + space_between_squares + x * square_size + space_between_squares * x,
+                                left_top_y + y * square_size + space_between_squares * y, square_size, square_size)
